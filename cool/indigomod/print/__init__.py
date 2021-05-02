6c4509683038b69db21b68a95be6298fd43dcf60
AcaiBerii <studiouifxdesignersandcoders@gmail.com>
2021-05-01 17:41:06 -0700

    cool'st

---
+++ cool/indigomod/print/__init__.py
@@ -0,0 +1,17 @@
+from cool.indigomod.get import *
+
+
+def out(msg):
+    print("[Indigo - Out] - " + msg)
+
+
+def warn(msg):
+    print("[Indigo - Warn] - " + msg)
+
+
+def err(msg):
+    print("[Indigo - Error] - " + msg)
+
+
+def notif(msg):
+    print("[Indigo - Message] - " + msg)
